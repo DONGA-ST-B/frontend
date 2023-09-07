193712372c8b19bcadc4022e735ee67f59c84ec2
arinming <jyandysys1@naver.com>
2023-09-08 02:40:21 +0900

    [feat] : 로고 추가, 배경 및 글꼴 색상 추가, 줄 간 여백 추가 (#3)

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import LogoWhite from "../../assets/Logo_White.png"
+import "./Footer.css";
+import { COLORS } from "../../styles/colors";
 
 // Footer에서 표시할 정보를 정의한 데이터 객체
 const footerData = {
@@ -23,16 +26,46 @@ const FooterInfo = ({ label, value }) => (
 
 class Footer extends Component {
   render() {
+    const footerStyle = {
+      backgroundColor: COLORS.GRAY_FOOTER_BG, // 배경 색상 설정
+      color: COLORS.GRAY_500, // 글자 색상 설정
+      padding: "20px", // 패딩 값 설정
+    };
+
+    const logoStyle = {
+      marginBottom: "54px",
+    };
+
+    const infoStyle = {
+      marginBottom: "32px",
+    };
+
+    const spacingStyle = {
+      marginBottom: "16px", // 추가한 여백 설정
+    };
+
     return (
-      <div>
+      <div style={footerStyle} className="footer-container">
+        <img src={LogoWhite} alt="로고" style={logoStyle} /> {/* 여백 적용 */}
+        <br />
+        <div style={infoStyle}>
         <FooterInfo label="사업자등록번호" value={footerData.registrationNumber} />
+        <div style={spacingStyle}></div>
         <FooterInfo label="상호" value={footerData.companyName} />
+        <div style={spacingStyle}></div>
         <FooterInfo label="대표자명" value={footerData.representativeName} />
+        <div style={spacingStyle}></div>
         <FooterInfo label="전화번호" value={<a href={footerData.email}>{footerData.phoneNumber}</a>} />
+        <div style={spacingStyle}></div>
         <FooterInfo label="주소" value={footerData.address} />
+        <div style={spacingStyle}></div>
         <FooterInfo label="통신판매업신고번호" value={footerData.salesRegistrationNumber} />
+        <div style={spacingStyle}></div>
         <FooterInfo label="의료기기판매업번호" value={footerData.deviceSalesNumber} />
-        <span className="copyright">{footerData.copyright}</span>
+        </div>
+        <div style={spacingStyle}></div>
+        <span className="copyright" style={{ color: COLORS.GRAY_500 }}>{footerData.copyright}</span>
+        <div style={spacingStyle}></div>
         <FooterInfo label="Hosting by" value={footerData.hosting} />
       </div>
     );
