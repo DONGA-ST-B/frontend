e56cc72021d187f743896dc66bbe48c478fbc44e
arinming <jyandysys1@naver.com>
2023-09-08 03:56:54 +0900

    [feat] : 메뉴 리스트 그룹을 컴포넌트로 구현 (#3)

--- src/pages/buyList/BuyListPage.js
+++ src/pages/buyList/BuyListPage.js
@@ -1,29 +1,12 @@
-import React, { Component } from "react";
-import BuyListButton from "../../components/buyList/BuyListButton"; // 컴포넌트의 경로에 맞게 수정하세요
+import React from "react";
+import BuyListButtonGroup from "../buyList/BuyListButtonGroup";
 
-class MyComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isSelected: false,
-    };
-  }
+export default function BuyListPage() {
+  const buttons = ["전체", "기기 본체", "추가 용품", "추가 서비스"];
 
-  handleButtonClick = () => {
-    this.setState({ isSelected: !this.state.isSelected });
-  };
-
-  render() {
-    return (
-      <div>
-        <BuyListButton
-          label="버튼"
-          onClick={this.handleButtonClick}
-          isSelected={this.state.isSelected}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <BuyListButtonGroup buttons={buttons} />
+    </div>
+  );
 }
-
-export default MyComponent;
