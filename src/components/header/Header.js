1c41b1ea3c545a8c4db5e434c4f585c4b4066bd7
chamny20 <chamny20@naver.com>
2023-09-08 10:42:19 +0900

    [Feat] : complete header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,12 +6,11 @@ export default function Header() {
   return (
     <HeaderBox>
       <LeftBox>
-        <a href="/">
-          {" "}
-          <img src={logo} alt="Hicardi" width="130px" />
-        </a>
-
         <MenuText>
+          <a href="/">
+            {" "}
+            <img src={logo} alt="Hicardi" width="130px" />
+          </a>
           <TextContent>서비스 소개</TextContent>
           <TextContent>구매하기</TextContent>
           <TextContent>FAQ</TextContent>
@@ -21,9 +20,9 @@ export default function Header() {
 
       <RightBox>
         <MenuText>
-          <TextContent>검색</TextContent>
-          <TextContent>로그인</TextContent>
-          <TextContent>회원가입</TextContent>
+          <WhiteButton>검색</WhiteButton>
+          <WhiteButton>로그인</WhiteButton>
+          <SkyButton>회원가입</SkyButton>
         </MenuText>
       </RightBox>
     </HeaderBox>
@@ -33,11 +32,12 @@ export default function Header() {
 const HeaderBox = styled.div`
   /* border: 2px solid red; */
   width: 80%;
-  height: 83px;
+  /* height: 83px; */
   margin: 0 auto;
   margin-top: 10px;
   margin-bottom: 10px;
   display: flex;
+
   justify-content: space-between;
 `;
 
@@ -51,15 +51,40 @@ const RightBox = styled.div`
   display: flex;
 `;
 
-const MenuText = styled.div`
+const MenuText = styled.ul`
   /* border: 1px solid black; */
   display: flex;
+  align-items: center;
   font-size: 0.8rem;
   font-weight: bold;
   margin: 10px;
+  padding: 5px;
 `;
 
-const TextContent = styled.div`
-  margin-left: 20px;
-  margin-right: 20px;
+const TextContent = styled.li`
+  margin-left: 15px;
+  margin-right: 15px;
+  list-style: none;
+  padding: 5px;
+`;
+
+const SkyButton = styled.div`
+  background-color: #01b0ef;
+  border-radius: 100px;
+  /* border: 1px solid black; */
+  padding: 6px 15px 6px 15px;
+  color: white;
+  margin-left: 7px;
+  margin-right: 7px;
+`;
+
+const WhiteButton = styled.div`
+  border: 1px solid #eee;
+  box-sizing: border-box;
+  border-radius: 100px;
+  background: #fff;
+  /* transition: all 0.3s; */
+  padding: 6px 15px 6px 15px;
+  margin-left: 7px;
+  margin-right: 7px;
 `;
