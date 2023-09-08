4c97a39f302ec07acbe98006feec308d1df42ed8
arinming <jyandysys1@naver.com>
2023-09-09 01:04:10 +0900

    [feat] : 텍스트 박스를 배너에 추가 (#8)

--- src/pages/buyList/BuyBanner.js
+++ src/pages/buyList/BuyBanner.js
@@ -1,18 +1,19 @@
 import React from "react";
 import BuyBannerImage from "../../assets/BuyBannerBG.png";
 import BannerImage from "../../assets/BannerImage.png";
+import BannerTextBox from "./BannerTextBox"; 
 
 const BuyBanner = () => {
   const bannerStyle = {
     height: "350px",
-    margin: "20px 50px", 
+    margin: "20px 50px",
     borderRadius: "40px",
     overflow: "hidden",
-    position: "relative", 
+    position: "relative",
   };
 
   const overlayImageStyle = {
-    position: "absolute", 
+    position: "absolute",
     top: 0,
     left: 0,
     width: "100%",
@@ -27,14 +28,21 @@ const BuyBanner = () => {
     height: "100%",
   };
 
+  const textBoxStyle = {
+    position: "absolute",
+    top: "120px", // 위 여백 120px
+    left: "80px", // 왼쪽 여백 80px
+    textAlign: "left", // 왼쪽 정렬 설정
+  };
+  
+
   return (
     <div style={bannerStyle}>
       <div style={backgroundStyle}></div>
-      <img
-        src={BuyBannerImage}
-        alt="Overlay"
-        style={overlayImageStyle}
-      />
+      <img src={BuyBannerImage} alt="Overlay" style={overlayImageStyle} />
+      <div style={textBoxStyle}>
+        <BannerTextBox />
+      </div>
     </div>
   );
 };
