ff8f4bfebd3fb2e4f0ae137acb7fb1e870e422cd
chamny20 <chamny20@naver.com>
2023-09-10 17:30:31 +0900

    [fix] : package.json 수정

--- src/pages/monitoring/ScrollAnimationContainer.tsx
+++ src/pages/monitoring/ScrollAnimationContainer.tsx
@@ -1,40 +1,40 @@
-import styled, { keyframes } from "styled-components";
-// import React from "react";
-import { UseScrollAnimation } from "./UseScroll.ts";
+// import styled, { keyframes } from "styled-components";
+// // import React from "react";
+// import { UseScrollAnimation } from "./UseScroll.ts";
 
-type PropsType = {
-  children: React.ReactNode;
-};
+// type PropsType = {
+//   children: React.ReactNode;
+// };
 
-export const ScrollAnimationContainer = ({ children }: PropsType) => {
-  const { ref, isInViewport } = UseScrollAnimation();
-  return (
-    <Container ref={ref} className={isInViewport ? "frame-in" : ""}>
-      {children}
-    </Container>
-  );
-};
-const frameInAnimation = keyframes`
-  0% {
-    opacity: 0;
-    transform: translateY(-30%);
-  }
+// export const ScrollAnimationContainer = ({ children }: PropsType) => {
+//   const { ref, isInViewport } = UseScrollAnimation();
+//   return (
+//     <Container ref={ref} className={isInViewport ? "frame-in" : ""}>
+//       {children}
+//     </Container>
+//   );
+// };
+// const frameInAnimation = keyframes`
+//   0% {
+//     opacity: 0;
+//     transform: translateY(-30%);
+//   }
 
-  100%{
-    opacity: 1;
-    transform: translateY(0%);
-  }
-`;
+//   100%{
+//     opacity: 1;
+//     transform: translateY(0%);
+//   }
+// `;
 
-const Container = styled.div`
-  display: flex;
-  /* flex-direction: column; */
-  /* justify-content: center; */
-  align-items: center;
-  width: 100%;
-  height: 100vh;
+// const Container = styled.div`
+//   display: flex;
+//   /* flex-direction: column; */
+//   /* justify-content: center; */
+//   align-items: center;
+//   width: 100%;
+//   height: 100vh;
 
-  &.frame-in {
-    animation: ${frameInAnimation} 2s forwards;
-  }
-`;
+//   &.frame-in {
+//     animation: ${frameInAnimation} 2s forwards;
+//   }
+// `;
