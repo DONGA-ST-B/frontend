dcbdf011a5917036b672139432999b4e7fc54099
arinming <jyandysys1@naver.com>
2023-09-11 14:32:43 +0900

    [feat] : 옵션 컴포넌트 제작

--- src/pages/product/ProductContainer.js
+++ src/pages/product/ProductContainer.js
@@ -1,27 +1,48 @@
 import React from "react";
 import ProductImage from "../../assets/ProductDetailIamge.png";
 import ProductTextBox from "./ProductTextBox";
+import ProductOption from "./ProductOption";
 
 const ContainerComponent = () => {
-    const productInfo = {
-        productName: "제품명",
-        productNameEnglish: "Product Name",
-        description: "설명설명설명",
-        price: "가격",
-    };
+  const productInfo = {
+    productName: "제품명",
+    productNameEnglish: "Product Name",
+    description: "설명설명설명",
+    price: "가격",
+  };
 
-    return (
-        <div className="container">
-            <div className="left-side">
-                <img src={ProductImage} alt="" width="800" height="800" />
-            </div>
-            <div className="right-side">
-                <div className="text-box">
-                    <ProductTextBox {...productInfo} />
-                </div>
-            </div>
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    padding: "0 200px",
+  };
+
+  const leftSideStyle = {
+    flex: 1,
+  };
+
+  const rightSideStyle = {
+    flex: 2,
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={leftSideStyle}>
+        <img src={ProductImage} alt="" width="800" height="800" />
+      </div>
+      <div style={rightSideStyle}>
+        <div className="text-box">
+          <ProductTextBox {...productInfo} />
+          <div style={{ marginTop: "16px" }}>
+            <ProductOption />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ContainerComponent;
--- src/pages/product/ProductTextBox.js
+++ src/pages/product/ProductTextBox.js
@@ -1,24 +1,26 @@
 import React from 'react';
+import { COLORS } from "../../styles/colors";
+
 
 function ProductBox({ productName, productNameEnglish, description, price }) {
   const productBoxStyle = {
     fontSize: '32px',
     fontWeight: 'bold',
-    color: '#333', // GRAY_900
+    color: COLORS.GRAY_900,
     marginBottom: '8px',
   };
 
   const productNameEnglishStyle = {
     fontSize: '24px',
     fontWeight: 'bold',
-    color: '#888', // GRAY_500
+    color: COLORS.GRAY_500,
     marginTop: '8px',
     marginBottom: '16px',
   };
 
   const productDescriptionStyle = {
     fontSize: '18px',
-    color: '#888', // GRAY_500
+    color: COLORS.GRAY_500,
     marginTop: '16px',
     marginBottom: '32px',
   };
@@ -26,7 +28,7 @@ function ProductBox({ productName, productNameEnglish, description, price }) {
   const productPriceStyle = {
     fontSize: '28px',
     fontWeight: 'bold',
-    color: '#333', // GRAY_900
+    color: COLORS.GRAY_900,
     marginTop: '32px',
   };
 
