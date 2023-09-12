55929d565f56622b2456ecaace7f524de277f748
chamny20 <chamny20@naver.com>
2023-09-12 15:35:34 +0900

    [feat] : add view5~6 (#17)

--- src/pages/holter/HolterPage.js
+++ src/pages/holter/HolterPage.js
@@ -4,6 +4,8 @@ import Part1 from "./Part1";
 import Part2 from "./Part2";
 import Part3 from "./Part3";
 import Part4 from "./Part4";
+import Part5 from "./Part5";
+import Part6 from "./Part6";
 
 export default function HolterPage() {
   return (
@@ -13,6 +15,8 @@ export default function HolterPage() {
       <Part2 />
       <Part3 />
       <Part4 />
+      <Part5 />
+      <Part6 />
     </div>
   );
 }
