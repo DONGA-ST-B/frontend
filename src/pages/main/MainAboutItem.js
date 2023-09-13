21a68c97b5691b0d15ded87a43a17e73ad4e2a22
arinming <jyandysys1@naver.com>
2023-09-13 20:17:11 +0900

    [feat] : 활용 사례 아이템 구현 후 그룹화

---
+++ src/pages/main/MainAboutItem.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { COLORS } from '../../styles/colors';
+
+const containerStyle = {
+  width: '425px',
+  height: '605px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'left',
+};
+
+const imageStyle = {
+  width: '425px',
+  height: '400px',
+  borderRadius: '20px',
+  marginBottom: '48px',
+};
+
+const titleStyle = {
+  color: COLORS.GRAY_900,
+  fontSize: '32px',
+  fontWeight: 'bold',
+  marginBottom: '24px',
+};
+
+const contentStyle = {
+  color: COLORS.GRAY_600,
+  fontSize: '16px',
+};
+
+const MainAboutItem = ({ image, title, content }) => {
+  return (
+    <div style={containerStyle}>
+      <img src={image} alt="Item" style={imageStyle} />
+      <h1 style={titleStyle}>{title}</h1>
+      <p style={contentStyle}>{content}</p>
+    </div>
+  );
+};
+
+export default MainAboutItem;
--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -3,20 +3,22 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import MainBanner from "./MainBanner";
 import MainFirstText from "./MainFirstText";
+import MainAboutItem from "./MainAboutItem";
+import MainAboutGroup from "./MainAboutGroup";
 
 
 export default function MainPage() {
     return (
         <div>
             <Header />
-            <div style={{ marginTop: "40px", marginBottom: "40px" }}>
+            <div style={{ marginTop: "40px", marginBottom: "180px" }}>
                 <MainBanner />
             </div>
             <div style={{ marginBottom: "40px" }}>
                 <MainFirstText />
             </div>
             <div style={{ marginBottom: "40px" }}>
-            {/* 컴포넌트 3 */}
+                <MainAboutGroup />
             </div>
             <div style={{ marginBottom: "40px" }}>
             {/* 컴포넌트 4 */}
