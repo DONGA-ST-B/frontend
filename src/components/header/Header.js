2fb515c48f6f4c45b7cc2657a9ff451821d3ba6e
chamny20 <chamny20@naver.com>
2023-09-14 16:17:57 +0900

    [feat] : 헤더 UI 보완 및 검색창 UI 구현 완료

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,45 +5,61 @@ import logo from "../../assets/hicardi_logo.png";
 import { Search } from "@mui/icons-material";
 
 export default function Header() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleLoginClick = () => {
     navigate("/login");
   };
-
 
   return (
     <HeaderBox>
       <LeftBox>
         <MenuText>
           <Link to="/">
-            <img src={logo} alt="Hicardi" width="130px" />
-            </Link>
-            <a>
-          </a>
+            <img src={logo} alt="Hicardi" width="180px" />
+          </Link>
+
           <TextContent>
-            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-              서비스 소개
-            </Link>
+            <StyledLink to="/">서비스 소개</StyledLink>
             <SubMenu>
-              <Link to="/monitoring-solution" className="sub-li">
+              <SubStyledLink to="/monitoring-solution" className="sub-li">
                 환자모니터링 솔루션
-              </Link>
-              <Link to="/holter-solution" className="sub-li">
+              </SubStyledLink>
+              <SubStyledLink to="/holter-solution" className="sub-li">
                 홀터 솔루션
-              </Link>
+              </SubStyledLink>
             </SubMenu>
           </TextContent>
           <TextContent>구매하기</TextContent>
           <TextContent>FAQ</TextContent>
-          <TextContent>게시판</TextContent>
+          <TextContent>
+            <StyledLink to="">게시판</StyledLink>
+            <SubMenu>
+              <SubStyledLink to="" className="sub-li">
+                공지사항
+              </SubStyledLink>
+              <SubStyledLink to="" className="sub-li">
+                이벤트
+              </SubStyledLink>{" "}
+              <SubStyledLink to="" className="sub-li">
+                FAQ
+              </SubStyledLink>{" "}
+              <SubStyledLink to="" className="sub-li">
+                1:1 문의
+              </SubStyledLink>
+            </SubMenu>
+          </TextContent>
         </MenuText>
       </LeftBox>
 
       <RightBox>
         <MenuText>
           <WhiteButton>
-            <Search style={{ height: "17px" }} />
+            <StyledInput
+              placeholder="찾으시는 제품명을 입력해주세요."
+              type="text"
+            />
+            <Search />
           </WhiteButton>
           <WhiteButton onClick={handleLoginClick}>로그인</WhiteButton>
           <SkyButton>회원가입</SkyButton>
@@ -55,11 +71,11 @@ export default function Header() {
 
 const HeaderBox = styled.div`
   /* border: 2px solid red; */
-  width: 80%;
-  height: 7vh;
+  width: 100%;
+  height: 113px;
   margin: 0 auto;
-  margin-top: 10px;
-  margin-bottom: 10px;
+  /* margin-top: 10px;
+  margin-bottom: 10px; */
   display: flex;
 
   justify-content: space-between;
@@ -68,6 +84,7 @@ const HeaderBox = styled.div`
 const LeftBox = styled.div`
   /* border: 2px solid blue; */
   display: flex;
+  margin-left: 50px;
 `;
 
 const RightBox = styled.div`
@@ -79,13 +96,24 @@ const MenuText = styled.ul`
   /* border: 1px solid black; */
   display: flex;
   align-items: center;
-  font-size: 13px;
+  font-size: 1rem;
   font-weight: bold;
   margin: 10px;
   padding: 5px;
 `;
 
 const SkyButton = styled.div`
+  color: var(--system-white, #fff);
+  text-align: center;
+
+  /* B1 b */
+  font-family: Pretendard;
+  font-size: 0.9rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 140%; /* 25.2px */
+  letter-spacing: 0.072px;
+
   background-color: #01b0ef;
   border-radius: 100px;
   /* border: 1px solid black; */
@@ -96,7 +124,18 @@ const SkyButton = styled.div`
 `;
 
 const WhiteButton = styled.div`
-  border: 1px solid #eee;
+  color: var(--gray-900, #15191d);
+  text-align: center;
+
+  /* B1 b */
+  font-family: Pretendard;
+  font-size: 0.9rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 140%; /* 25.2px */
+  letter-spacing: 0.072px;
+
+  border: 1px solid #d3d3d3;
   box-sizing: border-box;
   border-radius: 100px;
   background: #fff;
@@ -106,18 +145,23 @@ const WhiteButton = styled.div`
   justify-content: center;
   margin-left: 7px;
   margin-right: 7px;
-  /* width: 100%; */
+  align-items: center;
 
   &:hover {
     cursor: pointer;
   }
 `;
 
-
 const SubMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  //
+
   border: 1px solid #d3d3d3;
-  border-radius: 15px;
-  width: 130px;
+  border-radius: 0 0px 15px 15px;
+  font-size: 1rem;
+  font-weight: 500;
+
   display: none; /* 초기에는 숨깁니다. */
   position: absolute;
   background-color: white;
@@ -127,19 +171,70 @@ const SubMenu = styled.ul`
   padding: 10px;
   list-style: none;
   margin: 0;
-  top: 100%; /* 부모 요소 아래에 위치하도록 설정합니다. */
+  top: 110%; /* 부모 요소 아래에 위치하도록 설정합니다. */
   left: 0;
+
+  text-align: left;
 `;
 
 const TextContent = styled.li`
-  margin-left: 15px;
-  margin-right: 15px;
+  color: var(--gray-900, #15191d);
+  text-align: center;
+  /* border: 1px solid red; */
+  /* H3_B */
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: bolder;
+  font-weight: 900;
+  line-height: 160%; /* 32px */
+
+  margin-left: 50px;
+  /* margin-right: px; */
   list-style: none;
   padding: 5px;
   position: relative; /* 부모 요소로부터 상대 위치로 설정합니다. */
+  &:hover {
+    color: #18afdd;
+    cursor: pointer;
+  }
 
   /* hover 시 SubMenu를 보이도록 설정합니다. */
   &:hover ${SubMenu} {
-    display: block;
+    color: #18afdd;
+    display: inline-block;
   }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: ${(props) => props.textDecoration || "none"};
+  color: ${(props) => props.color || "inherit"};
+`;
+
+const SubStyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  display: flex;
+  white-space: nowrap;
+
+  &:hover {
+    color: #18afdd;
+  }
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  outline: none;
+
+  padding-left: 10px;
+  padding-right: 10px;
+  text-align: left;
+  color: var(--gray-400, #aeb3b8);
+
+  /* B2 */
+  font-family: Pretendard;
+  font-size: 0.8rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%; /* 25.6px */
+  letter-spacing: 0.064px;
 `;
