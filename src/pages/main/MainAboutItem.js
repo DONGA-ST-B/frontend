5ce596cdb5f9caadd0c4cc7fefebc8228887c16b
chamny20 <chamny20@naver.com>
2023-09-17 04:06:40 +0900

    [fix] : 컴포넌트2 너비 반응형 조정 (#68)

--- src/pages/main/MainAboutItem.js
+++ src/pages/main/MainAboutItem.js
@@ -1,31 +1,41 @@
-import React from 'react';
-import { COLORS } from '../../styles/colors';
+import React from "react";
+import { COLORS } from "../../styles/colors";
+import styled from "styled-components";
 
 const containerStyle = {
-  width: '400px',
-  height: '605px',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'left',
+  width: "22%",
+  // height: "605px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "left",
+  // border: "1px solid blue",
 };
 
 const imageStyle = {
-  width: '400px',
-  height: '400px',
-  borderRadius: '20px',
-  marginBottom: '48px',
+  // width: "400px",
+  // height: "400px",
+  borderRadius: "20px",
+  marginBottom: "48px",
+  objectFit: "cover",
+  boxShadow: "10px 10px 10px 0px rgba(0, 0, 0, 0.13)",
 };
 
 const titleStyle = {
   color: COLORS.GRAY_900,
-  fontSize: '32px',
-  fontWeight: 'bold',
-  marginBottom: '24px',
+  fontSize: "32px",
+  fontWeight: "bold",
+  marginBottom: "24px",
+
+  color: "var(--gray-900, #15191D)",
+
+  /* H1_B */
+  fontFamily: "Pretendard",
+  lineHeight: "160%" /* 44.8px */,
 };
 
 const contentStyle = {
   color: COLORS.GRAY_600,
-  fontSize: '16px',
+  fontSize: "16px",
 };
 
 const MainAboutItem = ({ image, title, content }) => {
@@ -33,9 +43,21 @@ const MainAboutItem = ({ image, title, content }) => {
     <div style={containerStyle}>
       <img src={image} alt="Item" style={imageStyle} />
       <h1 style={titleStyle}>{title}</h1>
-      <p style={contentStyle}>{content}</p>
+      <ContentText>{content}</ContentText>
     </div>
   );
 };
+
+const ContentText = styled.div`
+  color: var(--gray-600, #666b6f);
+
+  /* B1 */
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%; /* 28.8px */
+  letter-spacing: 0.072px;
+`;
 
 export default MainAboutItem;
