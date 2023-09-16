01628f3c197af0b84fd6a683156a8e7467c8314e
arinming <jyandysys1@naver.com>
2023-09-16 10:55:03 +0900

    [feat] : 전체 카테고리 api 요청 성공

--- src/pages/buyList/BuyList.js
+++ src/pages/buyList/BuyList.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import ProductImage from "../../assets/ProductItem.png";
 import ProductItem from "./ProductItem";
 
@@ -30,16 +31,36 @@ const productList = [
 ];
 
 const BuyList = () => {
-  return (
+  const [products, setProducts] = useState([]);
+  const category = "TOTAL";
+  const url = `https://www.kusitms28.shop/api/product/category/${category}`; // API 엔드포인트 URL을 설정하세요.
+
+  useEffect(() => {
+    // 데이터를 불러오는 함수를 정의합니다.
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        const data = response.data.data;
+        setProducts(data); // API로부터 받은 데이터를 상태에 저장합니다.
+      } catch (error) {
+        console.error("API 호출 중 오류 발생:", error);
+      }
+    };
+
+    fetchData(); // 데이터를 불러오는 함수를 실행합니다.
+  }, [url]); // useEffect는 URL이 변경될 때마다 호출됩니다.
+
+
+ return (
     <div style={styles.container}>
-      {productList.map((product, index) => (
+      {products.map((product, index) => (
         <ProductItem
           key={index}
-          name={product.name}
-          imageSrc={product.imageSrc}
-          description={product.description}
+          name={product.productName}
+          imageSrc={product.photoUrl}
+          description={product.additionalDescription}
           price={product.price}
-          index={index} // index 속성을 추가
+          index={index}
         />
       ))}
     </div>
