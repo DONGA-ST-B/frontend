a67dd5a5bf66fe02de121cee797c1a913f489122
chamny20 <chamny20@naver.com>
2023-09-17 16:39:03 +0900

    [fix] : 데모 솔루션페이지에 추가 및 디테일 보완 (#68)

--- src/pages/monitoring/MonitoringPage.js
+++ src/pages/monitoring/MonitoringPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import MonitoringInfo from "./MonitoringInfo";
+import FloatingButton from "../../components/demo/FloatingButton";
 
 export default function MonitoringPage() {
   return (
     <div>
       <Header />
       <MonitoringInfo />
+      <FloatingButton />
     </div>
   );
 }
