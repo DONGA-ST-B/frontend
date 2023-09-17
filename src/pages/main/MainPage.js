641dc5e4afb9e2f1f5f69990b19207e4db137f87
chamny20 <chamny20@naver.com>
2023-09-17 17:12:06 +0900

    [add] : react-reveal 라이브러리 try

--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import MainBanner from "./MainBanner";
@@ -12,20 +12,54 @@ import DemoModal from "../../components/demo/DemoModal";
 import InitialDemo from "../../components/demo/InitialDemo";
 import CompleteDemo from "../../components/demo/CompleteDemo";
 
+import "./Main.css";
+
 export default function MainPage() {
+  // const [isVisible, setIsVisible] = useState(false);
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     // 스크롤 위치를 확인하고, 특정 지점에 도달하면 isVisible 상태를 변경
+  //     const scrollY = window.scrollY || document.documentElement.scrollTop;
+  //     const triggerPoint = 200; // 원하는 스크롤 위치에서 나타나게 조절
+  //     setIsVisible(scrollY > triggerPoint);
+  //   };
+
+  //   // 스크롤 이벤트 리스너 등록
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
   return (
     <div>
       <Header />
-      <div style={{marginBottom: "180px" }}>
+      <div style={{ marginBottom: "180px" }}>
         <MainBanner />
       </div>
       <div style={{ marginBottom: "40px" }}>
         <MainFirstText />
       </div>
-      <div style={{ marginBottom: "40px", marginLeft: "40px", marginRight: "40px" }}>
+      <div
+        // className={isVisible ? "fade-in-out" : ""}
+        style={{
+          marginBottom: "40px",
+          marginLeft: "40px",
+          marginRight: "40px",
+        }}
+      >
         <MainAboutGroup />
       </div>
-      <div style={{ marginBottom: "40px", marginLeft: "40px", marginRight: "40px" }}>
+      <div
+        style={{
+          marginBottom: "40px",
+          marginLeft: "40px",
+          marginRight: "40px",
+        }}
+      >
         <MainSelectGroup />
       </div>
       <div style={{ marginBottom: "40px" }}>
